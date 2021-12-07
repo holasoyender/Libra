425bb1596be414ac995c5e72f76a3609a96bd0ce
holasoyender <javier.projectzafire@gmail.com>
2021-12-07 22:07:48 +0100

    Comprobar recordatorios cada 30 segundos

--- src/main/java/libra/Functions/Recordatorios.java
+++ src/main/java/libra/Functions/Recordatorios.java
@@ -39,7 +39,7 @@ public class Recordatorios {
 
                 try {
                     //noinspection BusyWait
-                    Thread.sleep(10000);
+                    Thread.sleep(30000);
                 } catch (InterruptedException ex) {
                     System.out.println("Error: " + ex.getMessage());
                 }
