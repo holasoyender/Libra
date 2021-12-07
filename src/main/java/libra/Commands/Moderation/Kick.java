da2b5aec372bd3968d0a191474b9aa2501f2c392
holasoyender <javier.projectzafire@gmail.com>
2021-12-07 13:43:04 +0100

    Comando Kick

--- src/main/java/libra/Commands/Moderation/Kick.java
+++ src/main/java/libra/Commands/Moderation/Kick.java
@@ -13,7 +13,7 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.bson.Document;
 
-public class Warn implements Command {
+public class Kick implements Command {
     @Override
     public void run(SlashCommandEvent context, Document Guild, Config config) {
 
@@ -29,7 +29,7 @@ public class Warn implements Command {
         OptionMapping NOMDOption = context.getOption("md");
 
         if (MemberOption == null) {
-            context.reply(config.getEmojis().Error + "Debes especificar un miembro a avisar").setEphemeral(true).queue();
+            context.reply(config.getEmojis().Error + "Debes especificar un miembro a expulsar").setEphemeral(true).queue();
             return;
         }
 
@@ -45,17 +45,17 @@ public class Warn implements Command {
         }
 
         if (Member.equals(context.getMember())) {
-            context.reply(config.getEmojis().Error + "No puedes avisarte a ti mismo").setEphemeral(true).queue();
+            context.reply(config.getEmojis().Error + "No puedes expulsarte a ti mismo").setEphemeral(true).queue();
             return;
         }
 
         if (Member.getId().equals(context.getJDA().getSelfUser().getId())) {
-            context.reply(config.getEmojis().Error + "No puedes avisarte a mi!").setEphemeral(true).queue();
+            context.reply(config.getEmojis().Error + "No puedes expulsarme a mi!").setEphemeral(true).queue();
             return;
         }
 
         if (!context.getMember().canInteract(Member)) {
-            context.reply(config.getEmojis().Error + "No tienes permisos para avisar a este miembro").setEphemeral(true).queue();
+            context.reply(config.getEmojis().Error + "No tienes permisos para expulsar a este miembro").setEphemeral(true).queue();
             return;
         }
 
@@ -66,7 +66,7 @@ public class Warn implements Command {
                 Member.getUser().getName(),
                 context.getUser().getName(),
                 Reason,
-                "Warn",
+                "Kick",
                 "N/A",
                 Time.getTime(),
                 Infractions.generateID(context.getGuild().getId())
@@ -80,38 +80,45 @@ public class Warn implements Command {
 
             String finalReason = Reason;
             try {
-                Member.getUser().openPrivateChannel().complete().sendMessage("Has sido avisado en el servidor **" + context.getGuild().getName() + "** por " + context.getMember().getAsMention() + " con la razón: `" + Reason + "`")
+                Member.getUser().openPrivateChannel().complete().sendMessage("Has sido expulsado del servidor **" + context.getGuild().getName() + "** por " + context.getMember().getAsMention() + " con la razón: `" + Reason + "`")
                         .queue(
-                                ok -> context.reply(config.getEmojis().Success + "Has avisado a " + Member.getAsMention() + " con la razón: `" + finalReason + "`.").setEphemeral(false).queue(),
-                                err -> context.reply(config.getEmojis().Success + "Has avisado a " + Member.getAsMention() + " con la razón: `" + finalReason + "`, pero no he podido notificarle.").setEphemeral(false).queue());
+                                ok -> context.reply(config.getEmojis().Success + "Has expulsado a " + Member.getAsMention() + " con la razón: `" + finalReason + "`.").setEphemeral(false).queue(),
+                                err -> context.reply(config.getEmojis().Success + "Has expulsado a " + Member.getAsMention() + " con la razón: `" + finalReason + "`, pero no he podido notificarle.").setEphemeral(false).queue());
 
             } catch (Exception e) {
-                context.reply(config.getEmojis().Success + "Has avisado a " + Member.getAsMention() + " con la razón: `" + finalReason + "`, pero no he podido notificarle.").setEphemeral(false).queue();
+                context.reply(config.getEmojis().Success + "Has expulsado a " + Member.getAsMention() + " con la razón: `" + finalReason + "`, pero no he podido notificarle.").setEphemeral(false).queue();
             }
 
         } else {
-            context.reply(config.getEmojis().Success + "Has avisado a " + Member.getAsMention() + " con la razón: `" + Reason + "`.").setEphemeral(false).queue();
+            context.reply(config.getEmojis().Success + "Has expulsado a " + Member.getAsMention() + " con la razón: `" + Reason + "`.").setEphemeral(false).queue();
         }
+
+        try {
+            context.getGuild().kick(Member).reason(Reason).queue();
+        }catch (Exception e){
+            context.reply(config.getEmojis().Error + "No se ha podido expulsar al miembro, comprueba que tengo los permisos necesarios!").setEphemeral(true).queue();
+        }
+
     }
 
     @Override
     public String getName() {
-        return "warn";
+        return "kick";
     }
 
     @Override
     public String getDescription() {
-        return "Avisar a un usuario con una razón";
+        return "Expulsa a un usuario del servidor";
     }
 
     @Override
     public String getUsage() {
-        return "warn <Usuario> <Razón>";
+        return "kick <Usuario> <Razón>";
     }
 
     @Override
     public String getPermissions() {
-        return "Gestionar servidor";
+        return "Expulsar miembros";
     }
 
     @Override
@@ -122,8 +129,8 @@ public class Warn implements Command {
     @Override
     public CommandData getSlashData() {
         return new CommandData(this.getName(), this.getDescription())
-                .addOptions(new OptionData(OptionType.USER, "miembro", "Miembro a avisar", true),
-                        new OptionData(OptionType.STRING, "razón", "Razón del aviso", false),
+                .addOptions(new OptionData(OptionType.USER, "miembro", "Miembro a expulsar", true),
+                        new OptionData(OptionType.STRING, "razón", "Razón del expulsar", false),
                         new OptionData(OptionType.BOOLEAN, "md", "Mandar o no mandar un mensaje directo al usuario sancionado", false));
     }
 }
--- src/main/java/libra/Utils/Command/CommandManager.java
+++ src/main/java/libra/Utils/Command/CommandManager.java
@@ -31,6 +31,7 @@ public class CommandManager {
     }
 
     public CommandManager() {
+        addCommand(new Kick());
         addCommand(new Warn());
         addCommand(new Clear());
         addCommand(new Deshabilitados(this));
